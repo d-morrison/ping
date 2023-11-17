526d9b53a89a1b59d5539cb49bcf57f004eb45d4
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2023-11-16 16:29:35 -0800

    plot ping results

--- data-raw/ping.R
+++ data-raw/ping.R
@@ -1,8 +1,9 @@
 temp = readLines("inst/extdata/ping_data.txt")
-
+library(lubridate)
+start_time = today() + hours(14)
 library(dplyr)
 ping = tibble(
-  obs = 1:length(temp),
+  time = start_time + (1:length(temp) |> lubridate::seconds()),
   ping = if_else(
     grepl(fixed = TRUE, pattern = "Request timeout",x = temp),
     Inf,
@@ -13,9 +14,11 @@ ping = tibble(
 )
 
 library(ggplot2)
-ggplot(ping, aes(x = obs, y = ping, col = gap, group = 1)) +
+plot1 = ggplot(ping, aes(x = time, y = ping)) +
   # scale_y_continuous(trans = scales::reciprocal_trans()) +
   geom_point() +
-  geom_line()
+  geom_line() +
+  xlab('time ping sent (approx)')
 
+plot1 |>  plotly::ggplotly() |> print()
 usethis::use_data(ping, overwrite = TRUE)
