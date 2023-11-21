7263b8fb42d819758af2aa6e9d16254069ce445d
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2023-11-21 14:13:57 -0800

    cleanup

--- R/graph_pings.R
+++ R/graph_pings.R
@@ -7,7 +7,6 @@ graph_pings = function(ping)
     geom_line() +
     xlab('time ping sent (approx)')
 
-  plot1 |>  plotly::ggplotly() |> print()
-  usethis::use_data(ping, overwrite = TRUE)
+  plot1
 
 }
--- data-raw/ping.R
+++ data-raw/ping.R
@@ -5,6 +5,14 @@ load_all()
 # ping = process_pings_file("inst/extdata/ping_data.txt")
 
 ping = process_pings_file(
+  file = "inst/extdata/ping_data.txt",
+  start_time = lubridate::as_datetime("2023-11-17 10:08:45")
+)
+
+ping |> graph_pings()
+
+
+ping = process_pings_file(
   file = "inst/extdata/data_2023-11-20.txt",
   start_time = lubridate::as_datetime("2023-11-20 10:08:45")
 )
